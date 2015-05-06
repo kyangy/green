eb8f9a10a14a2a035c2c8564b183d70c71a514f4
Kevin Yang <YOUR-EMAIL-ADDRESS>
2015-05-06 15:11:31 -0700

    included admin to users

--- app/controllers/pickups_controller.rb
+++ app/controllers/pickups_controller.rb
@@ -1,2 +1,17 @@
 class PickupsController < ApplicationController
+
+	def create
+		@pickup = current_user.pickups.build(pickup_params)
+		if @pickup.save
+			redirect_to user_path(current_user)
+		else
+			render :new
+		end
+	end
+
+	private 
+
+	def pickup_params
+		params.require(:pickup).permit(:date)
+	end
 end

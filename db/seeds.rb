da5fc642157bc9c398aa4f2883a47728db33f894
Kevin Yang <YOUR-EMAIL-ADDRESS>
2015-05-07 16:31:18 -0700

    adding admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.destroy_all
+
+User.create(first: "green", last: "green", email: "green@gmail.com", address: "green", phone_number: "green", password: "greengreen", admin: true)
